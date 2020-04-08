57e0db2dc7a8d74990c286776a8d23dd058f8640
Eduardo Budny Pereira <budny@mbp-de-eduardo.localdomain>
2020-04-07 23:25:23 -0300

    feature:Created Pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,49 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  {
+    path: 'main',
+    loadChildren: () =>
+      import('./pages/main/main.module').then((m) => m.MainPageModule),
+  },
+  {
+    path: 'produtos',
+    loadChildren: () =>
+      import('./pages/produtos/produtos.module').then(
+        (m) => m.ProdutosPageModule
+      ),
+  },
+  {
+    path: 'clientes',
+    loadChildren: () =>
+      import('./pages/clientes/clientes.module').then(
+        (m) => m.ClientesPageModule
+      ),
+  },
+  {
+    path: 'fornecedores',
+    loadChildren: () =>
+      import('./pages/fornecedores/fornecedores.module').then(
+        (m) => m.FornecedoresPageModule
+      ),
+  },
+  {
+    path: 'pedidos',
+    loadChildren: () =>
+      import('./pages/pedidos/pedidos.module').then((m) => m.PedidosPageModule),
+  },
+  {
+    path: 'vendas',
+    loadChildren: () =>
+      import('./pages/vendas/vendas.module').then((m) => m.VendasPageModule),
+  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
